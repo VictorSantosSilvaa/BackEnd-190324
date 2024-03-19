f784bb66bf40639844bf2d850f4f8942b548d652
VictorSantosSilvaa <157403216+VictorSantosSilvaa@users.noreply.github.com>
2024-03-19 14:50:02 -0300

    update

---
+++ projProduct/script.js
@@ -0,0 +1,19 @@
+//Importando a biblioteca express
+const express = require('express')
+//definindo variaveis
+const porta = 3001
+const app = express()
+//definindo uma variavel para a rota
+const routes = require('./src/routes/routes')
+
+/* O trecho de código significa que todos os arquivos estáticos contidos no diretório "public" 
+serão disponibilizados para os clientes quando acessarem a aplicação. Por exemplo, se houver um arquivo 
+"index.html" dentro de "public", ele estará acessível através da rota principal da aplicação 
+(por exemplo, "http://seusite.com/"). */
+app.use(express.static("public"))
+
+app.use('/', routes)
+app.listen(porta, () =>{
+    console.log("Servidor executando.")
+    console.log(`http://localhost:${porta}`)
+})
---
+++ projProduct/src/controllers/productController.js
@@ -0,0 +1,27 @@
+const productRepository = require('../services/productRepository')
+
+exports.getAllProducts = async (req, res) => {
+    try {
+        //Está pegando o produto e instanciando 
+        const products = await productRepository.getAllProducts();
+        res.status(200).json(products)
+    } catch (err) {
+        res.status(500).json({ error: err.toString() })
+    }
+}
+
+exports.getProductById = async (req, res) => {
+    try {
+        //(req.params.id) => ele está mandando o numero do id
+        const product = await productRepository.getProductById(req.params.id)
+        if (!product) {
+            res.status(404).json({ error: "Produto não encontrado." })
+        } else {
+            res.status(200).json(product)
+        }
+    } catch (err) {
+        res.status(500).json({ error: err.toString })
+    }
+}
+
+
