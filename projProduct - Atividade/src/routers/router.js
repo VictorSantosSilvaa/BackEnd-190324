60f396e6e3393e150ab0d66711740d8f4ad5603d
VictorSantosSilvaa <157403216+VictorSantosSilvaa@users.noreply.github.com>
2024-03-20 14:47:17 -0300

    Update router.js

--- projProduct - Atividade/src/routers/router.js
+++ projProduct - Atividade/src/routers/router.js
@@ -4,10 +4,18 @@ const productController = require('../controllers/productController')
 
 const router = Router()
 
+//Produtos
 router.get('/products', productController.getAllProducts);
 router.get('/products/:id', productController.getProductById);
 router.post('/products', productController.createProduct);
 router.put('/products/:id', productController.updateProduct);
 router.delete('/products/:id', productController.deletProduct);
+
+//Usuario
+router.get('/user',userController.getAllUser);
+router.get('/user/:id', userController.getUserByID);
+router.post('/user ', userController.createUser);
+router.put('/user/:id', userController.updateUser);
+router.delete('/user/:id', userController.deleteUser);
 
 module.exports = router
